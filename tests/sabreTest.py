c258b91a04ed1cbce58e35ad6aa3272c48d21b02
Wilke Klausing <klausing@campus.tu-berlin.de>
2023-07-31 18:52:00 +0200

    Encapsulated sabre and sabreNew. Test works fine.

--- tests/sabreTest.py
+++ tests/sabreTest.py
@@ -1,41 +1,20 @@
 import unittest
 
-from src.sabre_own import initSabre
+from src.sabre_encapsulated import init as initSabreNew
+from src.sabre import init as initSabre
 
-#import sys
-#from os.path import dirname, abspath
-#sys.path.append(dirname(abspath(__file__)))
+# /Users/prabu/git/sabre/.venv/bin/python -m tests.sabreTest
 
 class TestMainFunction(unittest.TestCase):
-    def test_dictionary_values(self):
-        result = initSabre()  # assuming the main_function returns the result dictionary
-        expected_values = {
-            "buffer_size": 25000,
-            "total_played_utility": 484.9654794493675,
-            "time_average_played_utility": 2.4359830955119453,
-            "total_played_bitrate": 579714,
-            "time_average_played_bitrate": 2911.905205778773,
-            "total_play_time": 597.252272,
-            "total_play_time_chunks": 199.08409066666667,
-            "total_rebuffer": 0.0,
-            "rebuffer_ratio": 0.0,
-            "time_average_rebuffer": 0.0,
-            "total_rebuffer_events": 0,
-            "time_average_rebuffer_events": 0.0,
-            "total_bitrate_change": 80654,
-            "time_average_bitrate_change": 405.1252901721904,
-            "total_log_bitrate_change": 30.65922013412642,
-            "time_average_log_bitrate_change": 0.15400135707207366,
-            "time_average_score": 2.4359830955119453,
-            "over_estimate_count": 88,
-            "over_estimate": 334.3471958953972,
-            "leq_estimate_count": 110,
-            "leq_estimate": 689.183814243396,
-            "estimate": -234.28114307059911,
-            "rampup_time": 3.252272,
-            "total_reaction_time": 61.26135999999942
-        }    
-        self.assertDictEqual(result, expected_values)
+    def test_checkAllAbrOptions(self):        
+        abrList = ['bola', 'bolae', 'throughput', 'dynamic' , 'dynamicdash']
+        movingAverageList = ['ewma', 'sliding']
+
+        for abrA in abrList:
+            for movingAvg in movingAverageList:
+                result_sabre_encapsulated = initSabreNew(moving_average = movingAvg, abrInput = abrA)
+                result_sabre = initSabre(moving_average = movingAvg, abrInput = abrA)
+                self.assertDictEqual(result_sabre_encapsulated, result_sabre)
 
 if __name__ == '__main__':
     unittest.main()
