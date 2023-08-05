9c01e01d2270e379eb83b53e0ded2bca46db9cf9
Wilke Klausing <klausing@campus.tu-berlin.de>
2023-08-05 14:46:11 +0200

    Added new sabreOriginal, because sabre.py doe not has correct output. New tests.

--- tests/sabreTest.py
+++ tests/sabreTest.py
@@ -1,20 +1,55 @@
 import unittest
+import sys
+sys.path.append('/Users/prabu/git/sabre')
 
-from src.sabre_encapsulated import init as initSabreNew
+from src.sabreEncapsulated import init as initSabreNew
 from src.sabre import init as initSabre
+from src.sabreOriginal import init as initSabreOriginal
 
-# /Users/prabu/git/sabre/.venv/bin/python -m tests.sabreTest
+'''
+/Users/prabu/git/sabre/.venv/bin/python -m tests.sabreTest
+'''
 
 class TestMainFunction(unittest.TestCase):
-    def test_checkAllAbrOptions(self):        
-        abrList = ['bola', 'bolae', 'throughput', 'dynamic' , 'dynamicdash']
-        movingAverageList = ['ewma', 'sliding']
 
-        for abrA in abrList:
-            for movingAvg in movingAverageList:
-                result_sabre_encapsulated = initSabreNew(moving_average = movingAvg, abrInput = abrA)
-                result_sabre = initSabre(moving_average = movingAvg, abrInput = abrA)
-                self.assertDictEqual(result_sabre_encapsulated, result_sabre)
+    originalResult = {
+            'buffer_size': 25000,
+            'total_played_utility': 484.9654794493675,
+            'time_average_played_utility': 2.4359830955119453,
+            'total_played_bitrate': 579714,
+            'time_average_played_bitrate': 2911.905205778773,
+            'total_play_time': 597.252272,
+            'total_play_time_chunks': 199.08409066666667,
+            'total_rebuffer': 0.0,
+            'rebuffer_ratio': 0.0,
+            'time_average_rebuffer': 0.0,
+            'total_rebuffer_events': 0,
+            'time_average_rebuffer_events': 0.0,
+            'total_bitrate_change': 80654,
+            'time_average_bitrate_change': 405.1252901721904,
+            'total_log_bitrate_change': 30.65922013412642,
+            'time_average_log_bitrate_change': 0.15400135707207366,
+            'time_average_score': 2.4359830955119453,
+            'total_reaction_time': 61.26135999999942,
+            'estimate': -234.28114307059911
+        }
+
+    def testCheckValuesWithOriginal(self):
+        '''
+        Testing original values against sabreOriginal.py
+        '''
+        resultSabreOriginal = initSabreOriginal()
+        for key in self.originalResult:
+            self.assertEqual(self.originalResult[key], resultSabreOriginal[key])
+
+    def testCheckValuesWithNew(self):
+        '''
+        Testing sabreOriginal.py against sabreEncapsulated.py
+        '''
+        resultSabreOriginal = initSabreNew()
+        for key in self.originalResult:
+            self.assertEqual(self.originalResult[key], resultSabreOriginal[key])
+
 
 if __name__ == '__main__':
     unittest.main()
