2ac2331f80544b22e8f387310e3555669022f42b
Wilke Klausing <klausing@campus.tu-berlin.de>
2023-11-18 18:44:40 +0100

    Starting with SabreV6. It should stop when it needs new network condition.

--- tests/sabreTest.py
+++ tests/sabreTest.py
@@ -7,7 +7,8 @@ from src.sabreV1 import init as initSabreV1 # Having this for testing purposes
 from src.sabreV2 import init as initSabreV2 # Encapsulation done
 from src.sabreV3 import Sabre as SabreV3
 from src.sabreV4 import Sabre as SabreV4
-from src.sabreV5 import Sabre as SabreV5 # Working on this
+from src.sabreV6 import Sabre as SabreV6
+# from src.sabreV5 import Sabre as SabreV5 # Working on this
 
 #from src.gymSabre import SabreGym
 
@@ -36,62 +37,96 @@ class TestMainFunction(unittest.TestCase):
             'estimate': -234.28114307059911
         }
 
-    # def testCheckValuesWithSabreV1(self):
-    #     '''
-    #     Testing original values against sabre.py
-    #     '''
-    #     resultSabreOriginal = initSabreV1()
-    #     for key in self.originalResult:
-    #         self.assertEqual(self.originalResult[key], resultSabreOriginal[key])
-
-    # def testCheckValuesAgainstSabreV2(self):
-    #     '''
-    #     Testing sabreV1.py against sabreV2.py
-    #     '''
-    #     abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
-    #     averageList = ['ewma', 'sliding']
-    #     for abr in abrList:
-    #         for average in averageList:
-    #             print('Testing: ', abr, average)
-    #             resultSabreV1 = initSabreV1(abr=abr, moving_average=average, verboseInput=False)
-    #             resultSabreV2 = initSabreV2(abr=abr, moving_average=average, verbose=False)
-                
-    #             for key in resultSabreV1:
-    #                 self.assertEqual(resultSabreV1[key], resultSabreV2[key])
-
-    # def testCheckValuesAgainstSabreV3(self):
-    #     '''
-    #     Testing sabreV2.py against sabreV3.py
-    #     '''
-    #     abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
-    #     averageList = ['ewma', 'sliding']
-    #     for abr in abrList:
-    #         for average in averageList:
-    #             print('Testing: ', abr, average)
-    #             resultSabreV2 = initSabreV2(abr=abr, moving_average=average, verbose=False)
-    #             resultSabreV3 = SabreV3(abr=abr, moving_average=average, verbose=False).testing()
-                
-    #             for key in resultSabreV2:
-    #                 self.assertEqual(resultSabreV2[key], resultSabreV3[key])
-
-    # def testCheckValuesAgainstSabreV4(self):
-    #     '''
-    #     Testing sabreV3.py against sabreV4.py
-    #     '''
-    #     abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
-    #     averageList = ['ewma', 'sliding']
-    #     for abr in abrList:
-    #         for average in averageList:
-    #             print('Testing: ', abr, average)
-    #             resultSabreV3 = SabreV3(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
-    #             resultSabreV4 = SabreV4(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
-    #             for key in resultSabreV3:
-    #                 print(key)
-    #                 self.assertEqual(resultSabreV3[key], resultSabreV4[key])
-
-    def testCheckValuesAgainstSabreV5(self):
+    def testCheckValuesWithSabreV1(self):
         '''
-        Testing sabreV4.py against sabreV5.py
+        Testing original values against sabre.py
+        '''
+        resultSabreOriginal = initSabreV1()
+        for key in self.originalResult:
+            self.assertEqual(self.originalResult[key], resultSabreOriginal[key])
+
+    def testCheckValuesAgainstSabreV2(self):
+        '''
+        Testing sabreV1.py against sabreV2.py
+        '''
+        abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
+        averageList = ['ewma', 'sliding']
+        for abr in abrList:
+            for average in averageList:
+                print('Testing: ', abr, average)
+                resultSabreV1 = initSabreV1(abr=abr, moving_average=average, verboseInput=False)
+                resultSabreV2 = initSabreV2(abr=abr, moving_average=average, verbose=False)
+                
+                for key in resultSabreV1:
+                    self.assertEqual(resultSabreV1[key], resultSabreV2[key])
+
+    def testCheckValuesAgainstSabreV3(self):
+        '''
+        Testing sabreV2.py against sabreV3.py
+        '''
+        abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
+        averageList = ['ewma', 'sliding']
+        for abr in abrList:
+            for average in averageList:
+                print('Testing: ', abr, average)
+                resultSabreV2 = initSabreV2(abr=abr, moving_average=average, verbose=False)
+                resultSabreV3 = SabreV3(abr=abr, moving_average=average, verbose=False).testing()
+                
+                for key in resultSabreV2:
+                    self.assertEqual(resultSabreV2[key], resultSabreV3[key])
+
+    def testCheckValuesAgainstSabreV4(self):
+        '''
+        Testing sabreV3.py against sabreV4.py
+        '''
+        abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
+        averageList = ['ewma', 'sliding']
+        for abr in abrList:
+            for average in averageList:
+                print('Testing: ', abr, average)
+                resultSabreV3 = SabreV3(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
+                resultSabreV4 = SabreV4(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
+                for key in resultSabreV3:
+                    print(key)
+                    self.assertEqual(resultSabreV3[key], resultSabreV4[key])
+
+    # def testCheckValuesAgainstSabreV5(self):
+    #     '''
+    #     Testing sabreV4.py against sabreV5.py
+    #     '''
+    #     abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
+    #     averageList = ['ewma', 'sliding']
+    #     for abr in abrList:
+    #         for average in averageList:
+    #             print('Testing: ', abr, average)
+    #             resultSabreV4 = SabreV4(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
+    #             resultSabreV5 = SabreV5(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
+    #             for key in resultSabreV4:
+    #                 print(key)
+    #                 self.assertEqual(resultSabreV4[key], resultSabreV5[key])
+
+    # def testQoECalc(self):
+    #     '''
+    #     Example of calculating QoE with Sabre.
+    #     '''
+    #     # Bitrate - ReBuffering - BitrateSwichingEvents
+    #     sabre = SabreV5(abr='bola', moving_average='ewma', verbose=False,  replace='right')
+    #     result = {}
+    #     while True:
+    #         if isinstance(result, dict) and len(result) > 10: break            
+    #         result = sabre.downloadSegment()
+
+    #     bitrate = result['total_played_bitrate']
+    #     rebuffer = result['total_rebuffer']
+    #     bitrateSwitchingEvents = result['total_bitrate_change']
+    #     QoE = bitrate - rebuffer - bitrateSwitchingEvents
+        
+    #     print('QoE: ', QoE)
+    #     self.assertEqual(1, 1)
+        
+    def testCheckValuesAgainstSabreV6(self):
+        '''
+        Testing sabreV4.py against sabreV6.py
         '''
         abrList = ['bola', 'bolae', 'throughput', 'dynamic', 'dynamicdash']
         averageList = ['ewma', 'sliding']
@@ -99,30 +134,11 @@ class TestMainFunction(unittest.TestCase):
             for average in averageList:
                 print('Testing: ', abr, average)
                 resultSabreV4 = SabreV4(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
-                resultSabreV5 = SabreV5(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
+                resultSabreV6 = SabreV6(abr=abr, moving_average=average, verbose=False,  replace='right').testing()
                 for key in resultSabreV4:
                     print(key)
-                    self.assertEqual(resultSabreV4[key], resultSabreV5[key])
+                    self.assertEqual(resultSabreV4[key], resultSabreV6[key])
 
-    def testQoECalc(self):
-        '''
-        Example of calculating QoE with Sabre.
-        '''
-        # Bitrate - ReBuffering - BitrateSwichingEvents
-        sabre = SabreV5(abr='bola', moving_average='ewma', verbose=False,  replace='right')
-        result = {}
-        while True:
-            if isinstance(result, dict) and len(result) > 10: break            
-            result = sabre.downloadSegment()
-
-        bitrate = result['total_played_bitrate']
-        rebuffer = result['total_rebuffer']
-        bitrateSwitchingEvents = result['total_bitrate_change']
-        QoE = bitrate - rebuffer - bitrateSwitchingEvents
-        
-        print('QoE: ', QoE)
-        self.assertEqual(1, 1)
-        
 
 if __name__ == '__main__':
     unittest.main()
